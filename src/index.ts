5154da99259c1bd98514879df299b8671cd973a8
LichLN <lichln@bsscommerce.com>
2025-08-08 14:54:41 +0700

    fix cors headers

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,7 @@ export default {
 		const corsHeaders = {
       "Access-Control-Allow-Origin": "*",
       "Access-Control-Allow-Methods": "GET,HEAD,POST,OPTIONS",
+      "Access-Control-Allow-Headers": "Content-Type",
       "Access-Control-Max-Age": "86400",
     };
 
